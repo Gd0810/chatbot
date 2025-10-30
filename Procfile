29fac16fe320bff69c156558a369338643e1ba2d
Gd0810 <gd8102000@gmail.com>
2025-10-30 16:35:01 +0530

    Final4

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn redbot.wsgi:application --bind 0.0.0.0:$PORT
+web: gunicorn redbot.wsgi:application --chdir .
--- redbot/settings.py
+++ redbot/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['redbot.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

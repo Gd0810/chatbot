1fff54550af3baab400d8d6a79c6e4ac3c49c21c
Gd0810 <zeefer2000@gmail.com>
2025-12-05 12:53:12 +0530

    update:bot switcher default setup in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 # accounts/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django import forms
 from .models import User, Workspace
 
 @admin.register(User)
@@ -10,8 +11,41 @@ class CustomUserAdmin(UserAdmin):
     search_fields = ['username', 'email']
 
 
+class WorkspaceAdminForm(forms.ModelForm):
+    """Custom form to dynamically filter default_bot_mode choices based on workspace plan."""
+    
+    class Meta:
+        model = Workspace
+        fields = '__all__'
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        # If editing an existing workspace, filter choices based on active plan
+        if self.instance and self.instance.pk:
+            available_modes = self.instance.get_available_bot_modes()
+            
+            # Only show the field if there are multiple bot modes available
+            if len(available_modes) > 1:
+                # Filter choices to only show available modes
+                all_choices = [('', '-- Auto (based on plan) --')]
+                for mode in available_modes:
+                    # Find the matching choice from DEFAULT_BOT_MODES
+                    for choice_value, choice_label in Workspace.DEFAULT_BOT_MODES:
+                        if choice_value == mode:
+                            all_choices.append((choice_value, choice_label))
+                            break
+                
+                self.fields['default_bot_mode'].choices = all_choices
+            else:
+                # Hide the field for single-bot plans
+                self.fields['default_bot_mode'].widget = forms.HiddenInput()
+                self.fields['default_bot_mode'].required = False
+
+
 @admin.register(Workspace)
 class WorkspaceAdmin(admin.ModelAdmin):
+    form = WorkspaceAdminForm
     list_display = ['name', 'owner', 'approved', 'operational', 'active_plan_bundle', 'created_at', 'enable_reset_button']
     list_filter = ['approved', 'owner', 'enable_reset_button']
     search_fields = ['name', 'owner__username']

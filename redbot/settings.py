b4026049878258e3a055420702e252de9249c4c7
Gd0810 <zeefer2000@gmail.com>
2025-12-25 10:00:41 +0530

    update:dashboard responsive

--- redbot/settings.py
+++ redbot/settings.py
@@ -31,7 +31,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '192.168.0.100',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 

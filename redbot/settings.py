d74be6051ed377e57c71048043be41b3a95e345c
gd0810 <zeefer2000@gmail.com>
2026-02-06 15:07:41 +0530

    update:sqlite_to_mysql

--- redbot/settings.py
+++ redbot/settings.py
@@ -125,8 +125,15 @@ WSGI_APPLICATION = 'redbot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'chatbot',         
+        'USER': 'root',                 
+        'PASSWORD': '',                 
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 

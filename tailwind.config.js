ce3b29068443260171689c94fbaef290c3d8937c
gd0810 <zeefer2000@gmail.com>
2026-03-31 12:05:06 +0530

    add:tailwind_build

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/pages/**/*.html",
+    "./templates/pages/includes/**/*.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "rgb(255, 60, 0)",
+        "primary-dark": "rgb(217, 38, 0)",
+        "primary-darker": "rgb(175, 20, 0)",
+        "primary-darkest": "rgb(67, 26, 0)"
+      },
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"]
+      }
+    }
+  },
+  plugins: []
+};
